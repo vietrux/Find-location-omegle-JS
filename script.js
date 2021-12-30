730248558475093724d61ed846ce5879ab4ec8d8
Việt Trung <89530449+vietrux85@users.noreply.github.com>
2021-12-30 12:35:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
-YOUR_TOKEN = 'XXXXXXXXXXXXXX' /* Lấy token ở https://ipinfo.io 
-                                 Get your token at https://ipinfo.io */
-
+YOUR_TOKEN = 'XXXXXXXXXXXXXX' /* Get your token here https://ipinfo.io */
 window.oRTCPeerConnection  = window.oRTCPeerConnection || window.RTCPeerConnection
+
 window.RTCPeerConnection = function(...args) {
  const pc = new window.oRTCPeerConnection(...args)
+
 pc.oaddIceCandidate = pc.addIceCandidate
+
 pc.addIceCandidate = function(iceCandidate, ...rest) {
  const fields = iceCandidate.candidate.split(' ')
+
 if (fields[7] === 'srflx') {
-console.log('IP Address:', fields[4])
+var today=new Date();
+var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
+var today = new Date();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 fetch('https://ipinfo.io/'+fields[4]+'?token='+YOUR_TOKEN)
         .then(res => res.json())
          .then(data => {
-              console.log('IP: '+data['ip']+'\n'+' HostName: '+data['hostname']+'\n'+' Tọa độ: '+data['loc']+'\n'+' Thành phố: '+data['city']+'\n'+' Tỉnh: '+data['region']+'\n'+' Quốc gia: '+data['country'])
+              console.log('Thời gian'+date+' '+time+'\n'+'IP: '+data['ip']+'\n'+' HostName: '+data['hostname']+'\n'+' Tọa độ: '+data['loc']+'\n'+' Thành phố: '+data['city']+'\n'+' Tỉnh: '+data['region']+'\n'+' Quốc gia: '+data['country'])
        })
 }
 return pc.oaddIceCandidate(iceCandidate, ...rest)
+
 }
+
 return pc
 }
