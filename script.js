733a0af9ca445ef47ec5d3b40aa8c229db60e503
Việt Trung <89530449+vietrux@users.noreply.github.com>
2022-08-09 01:44:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-YOUR_TOKEN = 'XXXXXXXXXXXXXX'; //token for ipinfo service //d01df29be7f3b2
-//Get IP 
+
 window.oRTCPeerConnection = window.oRTCPeerConnection || window.RTCPeerConnection;
 window.RTCPeerConnection = function (...args) {
-    const pc = new window.oRTCPeerConnection(...args);
-    pc.oaddIceCandidate = pc.addIceCandidate;
-    pc.addIceCandidate = function (iceCandidate, ...rest) {
-        const fields = iceCandidate.candidate.split(" ");
-        if (fields[7] === "srflx") {
-            // IP =  fields[4]
-            fetch('https://ipinfo.io/' + fields[4] + '?token=' + YOUR_TOKEN) //fetch data from ipinfo.io
-                .then(response => response.json())
-                .then(data => {
-                    let regionNames = new Intl.DisplayNames(['en'], { type: 'region' });
-                    console.log('Follow me on Github: vietrux\nLoc: ' + data.loc + '\nCity: ' + data.city + '\nRegion: ' + data.region + '\nCountry: ' + regionNames.of(data.country));
-                });
-        }
-        return pc.oaddIceCandidate(iceCandidate, ...rest);
-    };
-    return pc;
+  const pc = new window.oRTCPeerConnection(...args);
+  pc.oaddIceCandidate = pc.addIceCandidate;
+  pc.addIceCandidate = async function (iceCandidate, ...rest) {
+    const fields = iceCandidate.candidate.split(" ");
+    if (fields[7] === "srflx") {
+      // IP =  fields[4]
+      const url = 'https://corsproxy.io/?' + encodeURIComponent(`http://ip-api.com/json/${fields[4]}`);
+      const response = await fetch(url);
+      const json = await response.json();
+      var data = `
+latitude: ${json.lat},
+longitude: ${json.lon},
+city: ${json.city},
+region: ${json.regionName},
+country: ${json.country},
+      `
+      console.log(data);
+    }
+    return pc.oaddIceCandidate(iceCandidate, ...rest);
+  };
+  return pc;
 };
